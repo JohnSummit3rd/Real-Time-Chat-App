47117c7be7f04411cdb311766af084d9112a0e01
JohnSummit3rd <moyasu2005@gmail.com>
2026-05-10 17:31:29 +1000

    added in auth routes for signup and login

---
+++ server/routes/auth.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+// Signup
+
+router.post("/signup", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    // Check if user already exists
+    const existing = await User.findOne({ email });
+    if (existing) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // Hash the password
+    const hashed = await bcrypt.hash(password, 10);
+
+    // Create user
+    const user = new User({ username, email, password: hashed });
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error creating user" });
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User doesn't exist" });
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d",
+    });
+
+    res.json({ token, username: user.username });
+  } catch (err) {
+    res.status(500).json({ message: "Error logging in" });
+  }
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 const http = require("http");
 const path = require("path");
 const { Server } = require("socket.io");
+const mongoose = require("mongoose");
+const User = require("./models/User");
+
+mongoose.connect("mongodb+srv://moyasu2005_db_user:YD6dVgrIpj9CHsfQ@chattr.l8acmeq.mongodb.net/?appName=Chattr")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 // 1. Create Express app
 const app = express();
